2825b88f54980225de6ed2e3afa5c8400a0c1b0f
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-11-16 16:56:14 +0100

    added home page

---
+++ script.js
@@ -0,0 +1,7 @@
+// FAQ section
+const FAQs = document.querySelectorAll('.FAQ');
+FAQs.forEach(FAQ =>{
+    FAQ.addEventListener('click', function() {
+        FAQ.classList.toggle('showFAQ')
+    })
+})
